5d781800198148dd32a3be9e38e83bc208e53f42
Alireza Mohammadi <alireza17010@gmail.com>
2026-05-26 08:13:28 +0330

    Add Box2D-based collision interface

--- source/physics.hpp
+++ source/physics.hpp
@@ -1,12 +1,21 @@
+#ifndef _PHYSICS_HPP_
+#define _PHYSICS_HPP_
+
+#include <functional>
 #include <vector>
 #include "objects/object.hpp"
+
+struct CollisionBody;
 
 class Physics
 {
 public:
-    void gravity();
-    void collision(std::vector<Object *> objects);
-    void friction();
+    void collision(Object *obj,
+                   const std::vector<CollisionBody> &collisionBodies,
+                   const std::vector<Object *> &dynamicObjects,
+                   const std::function<void(Object *)> &hitEnemiesAbove);
 
 private:
 };
+
+#endif // !_PHYSICS_HPP_
